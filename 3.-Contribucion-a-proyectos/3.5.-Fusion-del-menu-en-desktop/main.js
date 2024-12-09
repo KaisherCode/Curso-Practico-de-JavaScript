620bd12d0c119ee56d4b6068c41e7a075fa7f23a
Wilson Dev <wilson.hch@hynkor.com>
2024-12-09 11:41:35 -0500

    remove name directories

--- 3.-Contribucion-a-proyectos/3.5.-Fusion-del-menu-en-desktop/main.js
+++
@@ -1,9 +0,0 @@
-const menuEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu');
-
-menuEmail.addEventListener('click',toggleDesktopMenu);
-
-function  toggleDesktopMenu(){
-    console.log('Click');
-    desktopMenu.classList.toggle('inactive');
-}
